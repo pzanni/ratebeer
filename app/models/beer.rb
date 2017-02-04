950abb9e5ffce16d1b5a966a931c91b50d8c9068
Anni Puurunen <puurunen.anni@gmail.com>
2017-02-04 02:25:20 +0200

    vko 2 loppuun

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,6 +1,6 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
-	has_many :ratings
+	has_many :ratings, dependent: :destroy
 
 	def average_rating 
 		sum = 0
@@ -9,6 +9,10 @@ class Beer < ActiveRecord::Base
 		end
 
 		sum/ratings.count
+	end
+
+	def to_s
+		"#{name} (#{brewery.name})"
 	end
 
 end
---
+++ app/models/concerns/rating_average.rb
@@ -0,0 +1,8 @@
+
+module RatingAverage
+  extend ActiveSupport::Concern
+
+  def average_rating
+    ratings.inject(0.0){ |sum, r| sum+r.score } / ratings.count
+  end
+end
